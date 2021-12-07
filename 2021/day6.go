b64020a879463bf0785ebe7ae48bbd2b5c706019
marcolussetti <git@marcolussetti.com>
2021-12-06 16:51:41 -0800

    2021/6: cleanup

--- 2021/day6.go
+++ 2021/day6.go
@@ -7,6 +7,29 @@ import (
 	"strings"
 )
 
+func countFishes(days int, fishes []int) int {
+	dayFishes := make([]int, days)
+
+	// Pre-seed the first round because it starts lower than 0
+	for fish := range fishes {
+		for newDay := fishes[fish]; newDay < days; newDay += 7 {
+			dayFishes[newDay] += 1
+		}
+	}
+	for day := range dayFishes {
+		for newDay := day + 9; newDay < days; newDay += 7 {
+			dayFishes[newDay] += dayFishes[day]
+		}
+	}
+
+	total := len(fishes)
+	for day := range dayFishes {
+		total += dayFishes[day]
+	}
+
+	return total
+}
+
 func main() {
 	dataString, _ := os.ReadFile("day6.txt")
 	dataSlice := strings.Split(string(dataString), ",")
@@ -16,40 +39,7 @@ func main() {
 		dataIntSlice = append(dataIntSlice, iInt)
 	}
 
-	// Test dataset
-	// dataIntSlice = []int{3, 4, 3, 1, 2}
+	fmt.Printf("Part A: %d\n", countFishes(80, dataIntSlice))
+	fmt.Printf("Part B: %d\n", countFishes(256, dataIntSlice))
 
-	// Only gets to day 228 before it dies out of memory
-	// for day := 0; day < 256; day++ {
-	// 	fmt.Println(day)
-	// 	for f := range dataIntSlice {
-	// 		if dataIntSlice[f] == 0 {
-	// 			dataIntSlice[f] = 6
-	// 			dataIntSlice = append(dataIntSlice, 8)
-	// 		} else {
-	// 			dataIntSlice[f] -= 1
-	// 		}
-	// 	}
-	// }
-	DAY_MAX := 256
-	dayFishes := make([]int, DAY_MAX)
-
-	// Pre-seed the first round because it starts lower than 0
-	for fish := range dataIntSlice {
-		for newDay := dataIntSlice[fish]; newDay < DAY_MAX; newDay += 7 {
-			dayFishes[newDay] += 1
-		}
-	}
-	for day := range dayFishes {
-		for newDay := day + 9; newDay < DAY_MAX; newDay += 7 {
-			dayFishes[newDay] += dayFishes[day]
-		}
-	}
-
-	total := len(dataIntSlice)
-	for day := range dayFishes {
-		total += dayFishes[day]
-	}
-
-	fmt.Print(total)
 }
